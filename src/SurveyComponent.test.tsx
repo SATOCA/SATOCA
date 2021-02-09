b33775adc3da35fbc04361390c505a23705069ba
egresser <j0_s3f@hotmail.de>
2021-02-10 00:10:13 +0100

    Cleanup last commit

--- src/SurveyComponent.test.tsx
+++ src/SurveyComponent.test.tsx
@@ -2,11 +2,11 @@ import { render } from "@testing-library/react";
 import { SurveyComponent } from "./SurveyComponent";
 
 test("render a SurveyComponent", () => {
-    const { getByTestId, getByRole } = render(<SurveyComponent id={2} />);
+  const { getByTestId, getByRole } = render(<SurveyComponent id={2} />);
 
-    const head = getByTestId("header");
-    expect(head).toHaveTextContent("Survey with id: 2");
+  const head = getByTestId("header");
+  expect(head).toHaveTextContent("Survey with id: 2");
 
-    const question = getByTestId("question");
-    expect(question).toBeInTheDocument();
+  const question = getByTestId("question");
+  expect(question).toBeInTheDocument();
 });
--- src/SurveyComponent.tsx
+++ src/SurveyComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { getSurveyFromMock } from "./Services/SurveyDataService";
 
@@ -9,16 +9,12 @@ function setupSurvey() {
   return survey.items;
 }
 
-//! \todo should have no items data -> items: {}
-const intialState = { finished: false, items: setupSurvey() };
-export type SurveyComponentState = Readonly<typeof intialState>;
-
 export type SurveyComponentProps = {
   id: number;
 };
 
-export function SurveyComponent(props: SurveyComponentProps){
-
+export function SurveyComponent(props: SurveyComponentProps) {
+  //! \todo should have no items data -> items: {}
   const [items, setItems] = useState(setupSurvey());
   let history = useHistory();
 
@@ -32,48 +28,12 @@ export function SurveyComponent(props: SurveyComponentProps){
   };
 
   return (
-      <div>
-        <h3 data-testid="header">Survey with id: {props.id}</h3>
-        {/* //! \todo replace with component (arg: item[0]) */}
-        <span data-testid="question" onClick={nextQuestion}>
-          {items[0].question.text}
-        </span>
-      </div>
+    <div>
+      <h3 data-testid="header">Survey with id: {props.id}</h3>
+      {/* //! \todo replace with component (arg: item[0]) */}
+      <span data-testid="question" onClick={nextQuestion}>
+        {items[0].question.text}
+      </span>
+    </div>
   );
 }
-
-// //! \todo setup state from props
-// export class SurveyComponent extends React.Component<
-//   SurveyComponentProps,
-//   SurveyComponentState
-// > {
-//   readonly state: SurveyComponentState = intialState;
-//
-//   nextQuestion = () => {
-//     if (this.state.items.length > 1) {
-//       //! \todo implement logic to select next item. for now the first item will be poped.
-//       this.setState({
-//         items: this.state.items.slice(1, this.state.items.length),
-//       });
-//     } else {
-//       let history = useHistory();
-//       history.push("survey-end");
-//       this.setState({ finished: true });
-//     }
-//   };
-//
-//   render() {
-//     return (
-//       <div>
-//         <h3 data-testid="header">Survey with id: {this.props.id}</h3>
-//         {/* //! \todo replace with component (arg: item[0]) */}
-//         <span data-testid="question" onClick={this.nextQuestion}>
-//           {this.state.items[0].question.text}
-//         </span>
-//         <p data-testid="finished">
-//           finished: {this.state.finished ? "yes" : "no"}
-//         </p>
-//       </div>
-//     );
-//   }
-// }
