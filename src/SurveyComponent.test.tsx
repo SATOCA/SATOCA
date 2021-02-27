c8710c44255cf0fc8aad761a8afdea1e9da1ce6f
egresser <j0_s3f@hotmail.de>
2021-02-27 03:03:19 +0100

    Allow usage of routing args for survey and unique survey
    #127

--- src/SurveyComponent.test.tsx
+++ src/SurveyComponent.test.tsx
@@ -1,11 +1,17 @@
 import { render } from "@testing-library/react";
+import React from "react";
 import { SurveyComponent } from "./SurveyComponent";
 
 test("render a SurveyComponent", () => {
-  const { getByTestId, getByRole } = render(<SurveyComponent id={2} />);
+  const { getByTestId } = render(
+    <SurveyComponent surveyId="3" uniqueSurveyId="4" />
+  );
 
   const head = getByTestId("header");
-  expect(head).toHaveTextContent("Survey with id: 2");
+  expect(head).toHaveTextContent("Survey with id: 3");
+
+  const head2 = getByTestId("header2");
+  expect(head2).toHaveTextContent("Unique Survey with id: 4");
 
   const question = getByTestId("question");
   expect(question).toBeInTheDocument();
--- src/SurveyComponent.tsx
+++ src/SurveyComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { RouteComponentProps, useHistory } from "react-router-dom";
 import { getSurveyFromMock } from "./Services/SurveyDataService";
 
 function setupSurvey() {
@@ -9,9 +9,13 @@ function setupSurvey() {
   return survey.items;
 }
 
-export type SurveyComponentProps = {
-  id: number;
-};
+type SurveyComponentProps = {
+  surveyId: string;
+  uniqueSurveyId: string;
+}
+
+export interface RouterSurveyComponentProps
+  extends RouteComponentProps<SurveyComponentProps> {}
 
 export function SurveyComponent(props: SurveyComponentProps) {
   //! \todo should have no items data -> items: {}
@@ -29,7 +33,10 @@ export function SurveyComponent(props: SurveyComponentProps) {
 
   return (
     <div>
-      <h3 data-testid="header">Survey with id: {props.id}</h3>
+      <h3 data-testid="header">Survey with id: {props.surveyId}</h3>
+      <h3 data-testid="header2">
+        Unique Survey with id: {props.uniqueSurveyId}
+      </h3>
       {/* //! \todo replace with component (arg: item[0]) */}
       <span data-testid="question" onClick={nextQuestion}>
         {items[0].question.text}
