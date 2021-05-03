d4a2d4a2fbfefeffa9f6b1dda6cd319423d96d63
michael miller <miller.michael@gmx.de>
2021-05-03 22:23:58 +0200

    add database password

--- Backend/src/App.ts
+++ Backend/src/App.ts
@@ -33,6 +33,7 @@ createConnection({
     "host": process.env.DATABASE_HOST!,
     "port": Number(process.env.DATABASE_PORT),
     "username": process.env.DATABASE_USERNAME!,
+    "password": process.env.DATABASE_PASSWORD!,
     "database": process.env.DATABASE_NAME!,
     "synchronize": true,
     "logging": false,
