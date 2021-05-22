2bea1cc5142eb98590c5d518cd380c2feb28871a
michael miller <miller.michael@gmx.de>
2021-05-22 12:14:52 +0200

    enable json parser for post requests

--- Backend/src/App.ts
+++ Backend/src/App.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import dotenv from "dotenv";
 import express, { Request, Response, NextFunction } from "express";
-import MainRouter from "./routers/MainRouter";
+import { MainRouter } from "./routers/MainRouter";
 import ErrorHandler from "./models/ErrorHandler";
 
 import { Survey } from "./entities/Survey";
@@ -11,19 +11,12 @@ import { Question } from "./entities/Question";
 import { Answer } from "./entities/Answer";
 import { FinishedQuestion } from "./entities/FinishedQuestion";
 
+const bodyParser = require('body-parser')
+
 // load the environment variables from the .env file
 dotenv.config({
   path: ".env",
 });
-
-/**
- * Express server application class.
- * @description Will later contain the routing system.
- */
-class Server {
-  public app = express();
-  public router = MainRouter;
-}
 
 // setup database connection
 createConnection({
@@ -43,13 +36,15 @@ createConnection({
     FinishedQuestion,
   ],
 }).then(async (connection) => {
-  // initialize server app
-  const server = new Server();
 
-  server.app.use("/api", server.router);
+  const app = express();
+  app.use(bodyParser.json());
+  const router = new MainRouter().router;
+
+  app.use("/api", router);
 
   // make server app handle any error
-  server.app.use(
+  app.use(
     (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
       res.status(err.statusCode || 500).json({
         status: "error",
@@ -61,6 +56,6 @@ createConnection({
 
   // make server listen on some port
   ((port = process.env.APP_PORT || 5000) => {
-    server.app.listen(port, () => console.log(`> Listening on port ${port}`));
+    app.listen(port, () => console.log(`> Listening on port ${port}`));
   })();
 });
