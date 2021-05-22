4ea8a5414e73559fd1387e73812c60d014173baa
michael miller <miller.michael@gmx.de>
2021-05-22 14:58:48 +0200

    allow access control from localhost

--- Backend/src/App.ts
+++ Backend/src/App.ts
@@ -41,6 +41,14 @@ createConnection({
   app.use(bodyParser.json());
   const router = new MainRouter().router;
 
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
   app.use("/api", router);
 
   // make server app handle any error
