7ec3043a5708f060d1daa0e031df2908cd9e631e
michael miller <miller.michael@gmx.de>
2021-05-22 12:19:13 +0200

    new entpoints for the survey

--- Backend/src/controllers/SurveyController.ts
+++ Backend/src/controllers/SurveyController.ts
@@ -5,27 +5,66 @@ import { getConnection } from "typeorm";
 import { FinishedQuestion } from "../entities/FinishedQuestion";
 import { Participant } from "../entities/Participant";
 import { ErrorDto } from "../routers/dto/ErrorDto";
+import { Survey } from "../entities/Survey";
+import { SurveyDto } from "../routers/dto/SurveyDto";
+import { SurveyResponseDto } from "../routers/dto/SurveyResponseDto";
 
 export class SurveyController {
+
+  async getSurveys() {
+    const query = await getConnection().getRepository(Survey).find();
+
+    const err: ErrorDto = {
+      message: query ? "" : "todo: error message",
+      hasError: query ? false : true,
+    };
+    const result: SurveyResponseDto = {
+      error: err,
+      surveys: query,
+    };
+    return result;
+  }
+
+  async addSurvey(body: SurveyDto) {
+    const obj = new Survey();
+    //! \todo title cannot be empty
+    obj.title = body.title;
+
+    let result: ErrorDto = {
+      message: "",
+      hasError: false,
+    };
+
+    getConnection().getRepository(Survey)
+      .save(obj)
+      .then(() => { result.hasError = false; })
+      .catch(e => {
+        result.hasError = true;
+        result.message = e;
+      });
+
+    return result;
+  }
+
+  //! \todo surveyId is not needed -> remove
   async getCurrentSurvey(surveyId: number, uniqueId: string) {
 
-    const progress = await getConnection()
+    const query = await getConnection()
       .getRepository(Participant)
-      .createQueryBuilder("progress")
-      .leftJoinAndSelect("progress.currentQuestion", "currentQuestion")
-      .innerJoinAndSelect("progress.participant", "participant")
+      .createQueryBuilder("participant")
+      .leftJoinAndSelect("participant.currentQuestion", "currentQuestion")
       .where("participant.uuid = :uuid", { uuid: uniqueId })
       .take(1)
       .getOne();
 
     const err: ErrorDto = {
-      message: progress ? "" : "todo: error message",
-      hasError: progress ? false : true,
+      message: query ? "" : "todo: error message",
+      hasError: query ? false : true,
     };
 
     const result: CurrentQuestionResponseDto = {
       error: err,
-      item: progress.currentQuestion,
+      item: query.currentQuestion
     };
     return result;
   }
@@ -33,19 +72,19 @@ export class SurveyController {
   async postCurrentSurvey(body: AnswerSurveyDto, surveyId: number, uniqueId: number) { // todo: uniqueId has to be string
 
     const progressQuery = await getConnection()
-        .getRepository(Participant)
-        .createQueryBuilder("participant")
-        .innerJoinAndSelect("participant.survey", "survey")
-        .innerJoinAndSelect("participant.currentQuestion", "currentQuestion")
-        .where("survey.Id = :surveyId", { surveyId: surveyId })
-        .andWhere("participant.uuid = :uniqueId", { uuid: uniqueId })
-        .getOneOrFail();
+      .getRepository(Participant)
+      .createQueryBuilder("participant")
+      .innerJoinAndSelect("participant.survey", "survey")
+      .innerJoinAndSelect("participant.currentQuestion", "currentQuestion")
+      .where("survey.Id = :surveyId", { surveyId: surveyId })
+      .andWhere("participant.uuid = :uniqueId", { uuid: uniqueId })
+      .getOneOrFail();
 
 
     let finishedQuestionRepository = getConnection().getRepository(FinishedQuestion);
     let finishedQuestion = new FinishedQuestion();
     finishedQuestion.id = body.itemId;
-    finishedQuestion.question = progressQuery.currentQuestion;
+    // todo: finishedQuestion.question = progressQuery.currentQuestion;
     finishedQuestion.givenAnswers = body.answers;
     await finishedQuestionRepository.save(finishedQuestion);
     console.log("submitted item: ", finishedQuestionRepository);
@@ -54,7 +93,7 @@ export class SurveyController {
     let participantRepository = getConnection().getRepository(Participant);
     // todo: replace answered currentQuestion with the next/following question
     let progress = new Participant();
-    progress.currentQuestion = progressQuery.currentQuestion;
+    // progress.currentQuestion = progressQuery.currentQuestion;
     await participantRepository.save(progress);
 
     let returnValue: AnswerSurveyResponseDto = {
--- Backend/src/routers/SurveyRouter.ts
+++ Backend/src/routers/SurveyRouter.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { SurveyController } from "../controllers/SurveyController";
+import { SurveyDto } from "./dto/SurveyDto";
 import { AnswerSurveyDto } from "./dto/AnswerSurveyDto";
 import ErrorHandler from "../models/ErrorHandler";
 export class SurveyRouter {
@@ -29,15 +30,34 @@ export class SurveyRouter {
     return { surveyId, uniqueId };
   }
 
-  /**
-   * Connect routes to their matching controller endpoints.
-   */
   private _configure() {
+    this._router.get(
+      "/all",
+      (req: Request, res: Response, next: NextFunction) => {
+        try {
+          this._controller.getSurveys().then(obj => { res.status(200).json(obj); });
+        } catch (error) {
+          next(error);
+        }
+      }
+    );
+
+    this._router.post(
+      "",
+      (req: Request, res: Response, next: NextFunction) => {
+        try {
+          this._controller.addSurvey(req.body as SurveyDto).then(obj => { res.status(200).json(obj); });
+        } catch (error) {
+          next(error);
+        }
+      }
+    );
+
     this._router.get(
       "/:surveyId/:uniqueId",
       (req: Request, res: Response, next: NextFunction) => {
         try {
-          this._controller.getCurrentSurvey(Number(req.params.surveyId), req.params.uniqueId).then(obj => { res.status(200).json(obj); });;
+          this._controller.getCurrentSurvey(Number(req.params.surveyId), req.params.uniqueId).then(obj => { res.status(200).json(obj); });
 
         } catch (error) {
           next(error);
