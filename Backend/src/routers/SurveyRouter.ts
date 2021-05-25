ea8fb20890932dbc1991b2c396ed0da620d01e9c
weissph64885 <weissph64885@th-nuernberg.de>
2021-05-25 21:46:00 +0200

    Modify UrlParam parser for post method

--- Backend/src/routers/SurveyRouter.ts
+++ Backend/src/routers/SurveyRouter.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response, Router } from "express";
 import { SurveyController } from "../controllers/SurveyController";
 import { SurveyDto } from "./dto/SurveyDto";
 import { AnswerSurveyDto } from "./dto/AnswerSurveyDto";
-import ErrorHandler from "../models/ErrorHandler";
+
 export class SurveyRouter {
   private _router = Router();
   private _controller = new SurveyController();
@@ -13,21 +13,6 @@ export class SurveyRouter {
 
   constructor() {
     this._configure();
-  }
-
-  private static extractUrlParams(
-    req: Request
-  ): { surveyId: number; uniqueId: number } {
-    if (!(req.params && req.params.surveyId && req.params.uniqueId))
-      throw new ErrorHandler(400, "Parameters invalid");
-
-    let surveyId = Number(req.params.surveyId);
-    let uniqueId = Number(req.params.uniqueId);
-
-    if (isNaN(surveyId) || isNaN(uniqueId))
-      throw new ErrorHandler(400, "Parameters invalid");
-
-    return { surveyId, uniqueId };
   }
 
   private _configure() {
@@ -58,7 +43,6 @@ export class SurveyRouter {
       (req: Request, res: Response, next: NextFunction) => {
         try {
           this._controller.getCurrentSurvey(Number(req.params.surveyId), req.params.uniqueId).then(obj => { res.status(200).json(obj); });
-
         } catch (error) {
           next(error);
         }
@@ -69,14 +53,8 @@ export class SurveyRouter {
       "/:surveyId/:uniqueId",
       (req: Request, res: Response, next: NextFunction) => {
         try {
-          let { surveyId, uniqueId } = SurveyRouter.extractUrlParams(req);
-
-          const result = this._controller.postCurrentSurvey(
-            req.body as AnswerSurveyDto,
-            surveyId,
-            uniqueId
-          );
-          res.status(200).json(result);
+          this._controller.postCurrentSurvey(req.body as AnswerSurveyDto, Number(req.params.surveyId), req.params.uniqueId)
+              .then(obj => { res.status(200).json(obj); });
         } catch (error) {
           next(error);
         }
