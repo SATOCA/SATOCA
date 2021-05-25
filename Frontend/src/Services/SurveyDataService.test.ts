4301bf4af0f5887bf05141653be3bfbc7fcbaa75
weissph64885 <weissph64885@th-nuernberg.de>
2021-05-25 19:17:07 +0200

    Fix SurveyDataService.test.ts

--- Frontend/src/Services/SurveyDataService.test.ts
+++ Frontend/src/Services/SurveyDataService.test.ts
@@ -4,16 +4,16 @@ test("Survey is returned", () => {
   const serviceData = SurveyDataService.getSurveyFromMock();
 
   expect(serviceData.items.length).toBe(3);
-  expect(serviceData.items[0].answerOptions.length).toBe(4);
-  expect(serviceData.items[1].answerOptions.length).toBe(6);
-  expect(serviceData.items[2].answerOptions.length).toBe(4);
+  expect(serviceData.items[0].choices.length).toBe(4);
+  expect(serviceData.items[1].choices.length).toBe(6);
+  expect(serviceData.items[2].choices.length).toBe(4);
 });
 
 test("test access to mockdata via wrapper function", () => {
   const survey = getSurveyFromMock();
 
   expect(survey.items.length).toBe(3);
-  expect(survey.items[0].answerOptions.length).toBe(4);
-  expect(survey.items[1].answerOptions.length).toBe(6);
-  expect(survey.items[2].answerOptions.length).toBe(4);
+  expect(survey.items[0].choices.length).toBe(4);
+  expect(survey.items[1].choices.length).toBe(6);
+  expect(survey.items[2].choices.length).toBe(4);
 });
