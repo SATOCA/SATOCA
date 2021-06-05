60d7a30a8e5860759edf5d235e55a7b9e7757713
bdrelisz <bdreliszak@gmail.com>
2021-06-05 12:49:03 +0200

    validation of login and password form if is not empty, deleting material-ui from json

--- Frontend/src/Components/TrusteeLogin/TrusteeLoginPage.tsx
+++ Frontend/src/Components/TrusteeLogin/TrusteeLoginPage.tsx
@@ -4,30 +4,32 @@ import {Button, Form, FormGroup, Input, Label} from "reactstrap";
 export default function TrusteeLogin() {
     const [login, setLogin] = React.useState("");
     const [password, setPassword] = React.useState("");
-    const [validLogin, setValidLogin] = React.useState(true);
-    const [validPassword, setValidPassword] = React.useState(true);
+    const [validLogin, setValidLogin] = React.useState(false);
+    const [validPassword, setValidPassword] = React.useState(false);
     const handleSubmit = (event: { currentTarget: any; preventDefault: () => void; stopPropagation: () => void; }) => {
         event.preventDefault();
         console.log(login,password);
-        if (login.length === 0) {
-            return;
-        }
-        if (password.length === 0) {
-            return;
-        }
+    };
+    const handleLoginInputChange = (e: { target: { name: any; value: any; }; }) => {
+        setLogin(e.target.value);
+        setValidLogin(e.target.value.length>0);
+    };
+    const handlePasswordInputChange = (e: { target: { name: any; value: any; }; }) => {
+        setPassword(e.target.value);
+        setValidPassword(e.target.value.length > 0);
     };
     return (
         <div className="centered front-page-alignment">
             <Form onSubmit={handleSubmit}>
                 <FormGroup>
                     <Label for="exampleEmail">Login</Label>
-                    <Input type="text" value={login} onChange={e => setLogin(e.target.value)}  />
+                    <Input type="text" value={login} onChange={handleLoginInputChange} valid={validLogin}/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="examplePassword">Password</Label>
-                    <Input type="text" value={password} onChange={e => setPassword(e.target.value)}  />
+                    <Input type="text" value={password} onChange={handlePasswordInputChange} valid={validPassword} />
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button disabled={validLogin===false || validPassword===false }>Submit</Button>
             </Form>
         </div>
     );
