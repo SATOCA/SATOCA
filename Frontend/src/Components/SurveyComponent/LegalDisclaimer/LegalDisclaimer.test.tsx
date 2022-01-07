83838c60cdb5e4ef7b9c4cb45c954d121eb5157a
michael miller <miller.michael@gmx.de>
2022-01-07 09:22:31 +0100

    fix test

--- Frontend/src/Components/SurveyComponent/LegalDisclaimer/LegalDisclaimer.test.tsx
+++ Frontend/src/Components/SurveyComponent/LegalDisclaimer/LegalDisclaimer.test.tsx
@@ -3,7 +3,10 @@ import { render, screen } from "@testing-library/react";
 import LegalDisclaimer from "./LegalDisclaimer";
 
 test("renders without crashing", () => {
-  render(<LegalDisclaimer surveyId={"1"}/>);
-  const finishedMessage = screen.getByText(/Legal Disclamer/i);
-  expect(finishedMessage).toBeInTheDocument();
+  render(<LegalDisclaimer surveyId={"1"}  participantId={"1"} />);
+  const checkBoxLabel = screen.getByText(/Accept legal disclaimer/i);
+  expect(checkBoxLabel).toBeInTheDocument();
+
+  const startButton = screen.getByText(/Start Survey/i);
+  expect(startButton).toBeInTheDocument();
 });
