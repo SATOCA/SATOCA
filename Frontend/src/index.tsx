5db1d2abbdfd2e22e7e8ffc37fb1fd72ad512238
michael miller <miller.michael@gmx.de>
2022-01-08 16:31:23 +0100

    using HashRouter to avoid 404 on reload

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
