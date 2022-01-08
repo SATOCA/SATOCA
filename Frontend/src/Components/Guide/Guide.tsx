32a39800b4ed5d4abfacb30a3fd0487ee41c28f8
bdrelisz <bdreliszak@gmail.com>
2022-01-08 17:47:40 +0100

    small changes to How to description

--- Frontend/src/Components/Guide/Guide.tsx
+++ Frontend/src/Components/Guide/Guide.tsx
@@ -17,51 +17,44 @@ export default function Guide() {
             <h4>How to Create a Survey?</h4>
             <p>Survey is uploaded from an Excel File. You will find an example in /docs Folder
                 under UploadSurvey.xlsx.
-              Document has two Sheets:
+            <p>Document has two Sheets:</p>
                 <li>Survey, where You save your questions</li>
                 <li>Options, where Survey parameters are defined.</li>
             </p>
               <img src={survey} alt={"survey"} />
-            <p> Survey Columns explanation</p>:
+            <p>Survey Columns explanation:</p>
               <ul>
-              <li>ID - question id</li>
-              <li>Question - question text</li>
-              <li>Solution - number of column with correct answer</li>
-              <li>Start Set - flag marking questions to begin the Survey with</li>
-              <li>Dificulty - question's difficulty parameter</li>
-              <li>Slope - question's slope/discrimination parameter</li>
-              <li>A1 - A5 - multiple choice answers</li>
+                <li>ID - question id</li>
+                <li>Question - question text</li>
+                <li>Solution - number of column with correct answer</li>
+                <li>Start Set - flag marking questions to begin the Survey with</li>
+                <li>Dificulty - question's difficulty parameter</li>
+                <li>Slope - question's slope/discrimination parameter</li>
+                <li>A1 - A5 - multiple choice answers</li>
               </ul>
             <img src={options} alt={"otpions"} />
-          <p>Options Columns explanation</p>:
+            <p>Options Columns explanation:</p>
               <ul>
-              <li>Title - survey title</li>
-              <li>Number participants - number of generated links
+                <li>Title - survey title</li>
+                <li>Number participants - number of generated links
                   to survey instances
-              </li>
-              <li>Item severity boundary - minimal information gain value</li>
-              <li>Privacy Budget - epsilon (ε) metric of privacy loss
+                </li>
+                <li>Item severity boundary - minimal information gain value</li>
+                <li>Privacy Budget - epsilon (ε) metric of privacy loss
                   at a differential change in data
-              </li>
-              <li>Legal Disclaimer - legal disclaimer text</li>
+                </li>
+                <li>Legal Disclaimer - legal disclaimer text</li>
               </ul>
-          <p>
-              To upload the Survey File Trustee have to login in.
+          <p>To upload the Survey File Trustee have to login in.</p>
+          <img src={login} alt={"login"} />
+          <p>Choose Survey File path.</p>
+          <img src={upload} alt={"upload"} />
+          <p>After uploading the file appropriately defined number
+              of links will be shown.
           </p>
-            <img src={login} alt={"login"} />
-          <p>
-              Choose Survey File path.
-          </p>
-            <img src={upload} alt={"upload"} />
-          <p>
-              After uploading the file appropriately defined (xlsx file)
-              number of links will be shown.
-          </p>
-            <img src={links} alt={"links"} />
-          <p>
-              For a started Survey Trustee can generate reports.
-          </p>
-            <img src={report} alt={"report"} />
+          <img src={links} alt={"links"} />
+          <p>For a started Survey Trustee can generate reports.</p>
+          <img src={report} alt={"report"} />
       </Container>
     </>
   );
