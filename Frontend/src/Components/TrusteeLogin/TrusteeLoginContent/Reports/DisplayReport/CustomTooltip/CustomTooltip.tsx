f0896965273f86700277dfe8e21bba628b857cac
egresser <j0_s3f@hotmail.de>
2022-01-16 21:42:33 +0100

    Change percentage display to match survey progress display

--- Frontend/src/Components/TrusteeLogin/TrusteeLoginContent/Reports/DisplayReport/CustomTooltip/CustomTooltip.tsx
+++ Frontend/src/Components/TrusteeLogin/TrusteeLoginContent/Reports/DisplayReport/CustomTooltip/CustomTooltip.tsx
@@ -11,22 +11,14 @@ export default function CustomTooltip({
   const parsePercentage = (input: any, precision: number) : string => {
     if (input === undefined)
       return "---";
-    return round(input, precision);
-  };
-
-  // see https://stackoverflow.com/a/7343013
-  const round = (value: number, precision: number = 0): string => {
-    const multiplier = Math.pow(10, precision);
-    const result = Math.round(value * multiplier) / multiplier;
-
-    return result.toFixed(precision);
+    return input.toPrecision(precision);
   };
 
   if (active) {
     return (
       <div className="custom-tooltip">
         <p className="tooltip-range">{`Range: ${label}`}</p>
-        <p className="tooltip-share">{`Share of participants: ${parsePercentage(payload?.[0].value, 2)}%`}</p>
+        <p className="tooltip-share">{`Share of participants: ${parsePercentage(payload?.[0].value, 3)}%`}</p>
       </div>
     );
   }
