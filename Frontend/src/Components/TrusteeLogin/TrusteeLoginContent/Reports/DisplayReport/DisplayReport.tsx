3bf6d010596b63ad8480350706538428544f21df
Max Wojtkow <Max.wojtkow@hotmail.de>
2022-01-18 01:01:53 +0100

    Commit by Philipp and Max: - fix statement

--- Frontend/src/Components/TrusteeLogin/TrusteeLoginContent/Reports/DisplayReport/DisplayReport.tsx
+++ Frontend/src/Components/TrusteeLogin/TrusteeLoginContent/Reports/DisplayReport/DisplayReport.tsx
@@ -74,46 +74,44 @@ export default function DisplayReport(props: {
     csvExporter.generateCsv(JSON.parse(JSON.stringify(data)));
   };
 
-  const reportChart = (data: HistogramData[]) => {
-    return (
-      <div className="histogram">
-        <ResponsiveContainer width="100%" height="90%">
-          <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 135,
-              left: 10,
-              bottom: 20,
+  const reportChart = (data: HistogramData[]) => (
+    <div className="histogram">
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 135,
+            left: 10,
+            bottom: 20,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            label={{
+              value: "Scoring",
+              position: "insideBottom",
+              dy: 20,
             }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              label={{
-                value: "Scoring",
-                position: "insideBottom",
-                dy: 20,
-              }}
-              dataKey="bucketName"
-            />
-            <YAxis
-              label={{
-                value: "Ratio [in %] ",
-                dx: -5,
-                angle: -90,
-                position: "insideLeft",
-              }}
-            />
-              <Tooltip content={<CustomTooltip />} />
-            <ReferenceLine y={0} stroke="#000" />
-            <Bar dataKey="score" fill="#0055a2" label="Test" />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
-    );
-  };
+            dataKey="bucketName"
+          />
+          <YAxis
+            label={{
+              value: "Ratio [in %] ",
+              dx: -5,
+              angle: -90,
+              position: "insideLeft",
+            }}
+          />
+          <Tooltip content={<CustomTooltip />} />
+          <ReferenceLine y={0} stroke="#000" />
+          <Bar dataKey="score" fill="#0055a2" label="Test" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 
   return (
     <Container className="glass-card-content" fluid="lg">
